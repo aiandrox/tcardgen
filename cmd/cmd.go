ef937ac60f61557fe15c27c9b230b0fca98719fb
Aiandrox <aiandrox7@gmail.com>
2020-09-21 17:18:15 +0900

    [Change] 著者ではなく管理者名を取得する 日付フォーマットの変更

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -174,7 +174,7 @@ func generateTCard(contentPath, outPath string, tpl image.Image, ffa *fontfamily
 		return err
 	}
 	if err := c.DrawTextAtPoint(
-		fmt.Sprintf("%s・%s", fm.Author, fm.Date.Format("Jan 2")),
+		fmt.Sprintf("%s / %s", $.Param "author", fm.Date.Format("2006-01-02")),
 		*cnf.Info.Start,
 		canvas.FgHexColor(cnf.Info.FgHexColor),
 		canvas.FontFaceFromFFA(ffa, cnf.Info.FontStyle, cnf.Info.FontSize),
